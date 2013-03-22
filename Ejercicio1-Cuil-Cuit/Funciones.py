a00325dfa6d09d002fbae2272a3b16a03c0ed4dc
jcruzrg <rgjuancruz@gmail.com>
2013-03-22 09:56:36 -0700

    Se mejoro la validacion e ingreso de datos

--- Ejercicio1-Cuil-Cuit/Funciones.py
+++ Ejercicio1-Cuil-Cuit/Funciones.py
@@ -4,32 +4,37 @@ Created on Mar 21, 2013
 @author: jc
 '''
 
+
 class MyClass(object):
     '''
     classdocs
     '''
-def valor1(codigo):
-    tuplanumeros = 5,4,3,2,7,6,5,4,3,2 #Tupla con numeros a multiplicar
+
+
+def averiguaciondelcuil(codigo, cuilcuit):
+    tuplanumeros = 5, 4, 3, 2, 7, 6, 5, 4, 3, 2  # Numerosmultiplicar
     sumar = 0
-    multivalor = 0
-    dni = "" #Limpiar dni
-    
-    while True: #Verificar el Dni o Numero de empresa
+    multivalor = 0  # Se utiliza para multiplicar la tupla con el dni
+    dni = ""   # Limpiar dni
+    while True:  # Verificar el Dni o Numero de empresa
         dni = raw_input("Ingrese el dni o el numero de la empresa")
         if len(dni) == 8:
             cuil = codigo + dni
             cuilformato = codigo + "-" + dni
-            cont = 0 #Inicializar cont
-            dni = codigo + dni #Dni + codigo de sexo
-            while cont < len(dni):   
-                multivalor = int(dni[cont]) * int(tuplanumeros[cont])
-                sumar = sumar + multivalor
-                cont = cont + 1
-            break
+            cont = 0  # Inicializar cont
+            dni = codigo + dni  # Dni + codigo de sexo
+            try:  # Verifica si en el numero de dni o empresa
+                # se ingresaron caracteres no validos
+                while cont < len(dni):
+                    multivalor = int(dni[cont]) * int(tuplanumeros[cont])
+                    sumar = sumar + multivalor
+                    cont = cont + 1
+                break
+            except Exception:
+                        pass
         print "Error al ingresar Dni"
         print ""
-            
-    division = sumar % 11    
+    division = sumar % 11
     codverificador = 11 - division
     if codverificador == 10:
         codverificador = 9
@@ -37,4 +42,7 @@ def valor1(codigo):
         codverificador = 0
     cuilformato = cuilformato + "-" + str(codverificador)
     cuil = cuil + str(codverificador)
-    print "Cuil= " + cuilformato    
+    if cuilcuit == 0:
+        print "Cuit= " + cuilformato
+    else:
+        print "Cuil= " + cuilformato
--- Ejercicio1-Cuil-Cuit/Main.py
+++ Ejercicio1-Cuil-Cuit/Main.py
@@ -3,14 +3,21 @@ Created on Mar 21, 2013
 
 @author: jc
 '''
-import Funciones #Importar clase
 
-hombremujerempresa = "" #Limpiar hombremujerempresa
-codident = 0 #Limpiar codigo de identificador
+
+import Funciones  # Importar clase
+
+hombremujerempresa = ""  # Limpiar hombremujerempresa
+codident = 0  # Limpiar codigo de identificador
+verempresa = 1  # Codigo para verificar si es cuil o cuit
 
 print "Hola..."
-while True: #Se verifica si lo que se ingreso es valido y de un solo caracter
-    hombremujerempresa = raw_input("Ingrese:\nM si es mujer\nV si es varon\nE si es empresa")
+
+while True:  # Se verifica si lo que se
+            # ingreso es valido y de un solo caracter
+    hombremujerempresa = raw_input("Ingrese:\
+    \nM si es mujer\nV si es varon\nE si es empresa")
+
     if len(hombremujerempresa) == 1:
         if hombremujerempresa.lower() == "m":
             codident = "27"
@@ -18,9 +25,12 @@ while True: #Se verifica si lo que se ingreso es valido y de un solo caracter
             codident = "20"
         elif hombremujerempresa.lower() == "e":
             codident = "30"
+            verempresa = 0
+        elif hombremujerempresa >= 0:
+            print "Error, ingrese lo pedido"
+            continue
         break
     print "Error, ingrese lo pedido"
 
-Funciones.valor1(codident) #Se le pasa el valor de los 2 primeros numero de identificacion
-
-
+Funciones.averiguaciondelcuil(codident, verempresa)  # Se le pasa el valor
+                            # de los 2 primeros numeros de identificacion
